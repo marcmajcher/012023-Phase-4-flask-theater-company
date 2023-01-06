809c685548706daea37d6e1e3dd3651eb587fa63
ixnp <enprocopios@gmail.com>
2023-01-06 10:39:28 -0800

    solution

--- 05-Auth-pt1/server/app.py
+++ 05-Auth-pt1/server/app.py
@@ -29,7 +29,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
-app.secret_key = b'?w\x85Z\x08Q\xbdO\xb8\xa9\xb65Kj\xa9_'
+app.secret_key = b'@~xH\xf2\x10k\x07hp\x85\xa6N\xde\xd4\xcd'
 
 migrate = Migrate(app, db)
 db.init_app(app)
