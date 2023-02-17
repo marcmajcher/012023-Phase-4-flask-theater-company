1b300a0985bc5f34ffdae46b85d29c3c9e1ffa35
Louis <Louis.Medina@flatironschool.com>
2023-02-17 15:50:10 -0400

    add more explicit steps to app.py

--- 05-Auth-pt1/server/app.py
+++ 05-Auth-pt1/server/app.py
@@ -165,7 +165,7 @@ api.add_resource(ProductionByID, '/productions/<int:id>')
 
 # 7.✅ Navigate to client/src/components/Navigation.js to build the logout button!
 
-# 8.✅ We will be using sessions in the application, so let's build out a quick cookie example
+# 8 (or 1).✅ We will be using sessions in the application, so let's build out a quick cookie example
     # Creating a non RESTful route for /dark_mode
         # 8.1 Use the @app.route decorator and pass it the path '/dark_mode' and the 'methods=['GET']'
         # 8.2 Create a method called dark mode. 
