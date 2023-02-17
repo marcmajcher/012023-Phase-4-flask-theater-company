c53a344ef09e71d11453d65c1e5d40e21c791932
Louis <Louis.Medina@flatironschool.com>
2023-02-17 13:54:15 -0400

    update numbering of comments in app.py

--- 05-Auth-pt1/server/app.py
+++ 05-Auth-pt1/server/app.py
@@ -121,14 +121,15 @@ api.add_resource(ProductionByID, '/productions/<int:id>')
 
 # 1.✅ User
     #A user model was added to "models.py" along with an Authentication component in client/src/components/Authentication.sj
-    # 2.1 Create a User POST route by creating a class Users that inherits from Resource
-    # 2.2 Add the route '/users' with api.add_resource()
-    # 2.3 Create a POST method
-        # 2.3.1 use .get_json() to convert the request json 
-        # 2.3.2 create a new user with the request data
-        # 2.3.3 add and commit the new user
-        # 2.3.4 Save the new users id to the session hash
-        # 2.3.5 Make a response and send it back to the client
+    # 1.1 Create a User POST route by creating a class Users that inherits from Resource
+    # 1.2 Add the route '/users' with api.add_resource()
+    # 1.3 Create a POST method
+        # 1.3.1 use .get_json() to convert the request json 
+        # 1.3.2 create a new user with the request data
+        # 1.3.3 add and commit the new user
+        # 1.3.4 Save the new users id to the session hash
+        # 1.3.5 Make a response and send it back to the client
+
 # 2.✅ Test this route in the client/src/components/Authentication.sj 
 class Users(Resource):
     def post(self):
@@ -153,10 +154,10 @@ api.add_resource(Users, '/users')
     # 3.1 Create a login class that inherits from Resource
     # 3.2 Use api.add_resource to add the '/login' path
     # 3.3 Build out the post method
-        # 4.3.1 convert the request from json and select the user name sent form the client. 
-        # 4.3.2 Use the name to query the user with a .filter
-        # 4.3.3 If found set the user_id to the session hash
-        # 4.3.4 convert the user to_dict and send a response back to the client 
+        # 3.3.1 convert the request from json and select the user name sent form the client. 
+        # 3.3.2 Use the name to query the user with a .filter
+        # 3.3.3 If found set the user_id to the session hash
+        # 3.3.4 convert the user to_dict and send a response back to the client 
     #3.4 Toggle the signup form to login and test the login route
 
 
@@ -202,7 +203,7 @@ api.add_resource(AuthorizedSession, '/authorized')
     # 6.3 Clear the user id in session by setting the key to None
     # 6.4 create a 204 no content response to send back to the client
 
-# 6.✅ Navigate to client/src/components/Navigation.js to build the logout button!
+# 7.✅ Navigate to client/src/components/Navigation.js to build the logout button!
 class Logout(Resource):
     def delete(self):
         session['user_id'] = None
@@ -212,13 +213,13 @@ class Logout(Resource):
 api.add_resource(Logout, '/logout')
 
 
-# 7.✅ We will be using sessions in the application, so let's build out a quick cookie example
+# 8.✅ We will be using sessions in the application, so let's build out a quick cookie example
     # Creating a non RESTful route for /dark_mode
-        # 7.1 Use the @app.route decorator and pass it the path '/dark_mode' and the 'methods=['GET']'
-        # 7.2 Create a method called dark mode. 
-        # 7.3 Create a response with make_response and pass it a dict that will list all of our cookies jsonify
-        # 7.4 Set the cookies in the response with set_cookie and pass it a key 'mode' and a value 'dark'
-        # 7.5 return the response, run the server and check the response in the browser.
+        # 8.1 Use the @app.route decorator and pass it the path '/dark_mode' and the 'methods=['GET']'
+        # 8.2 Create a method called dark mode. 
+        # 8.3 Create a response with make_response and pass it a dict that will list all of our cookies jsonify
+        # 8.4 Set the cookies in the response with set_cookie and pass it a key 'mode' and a value 'dark'
+        # 8.5 return the response, run the server and check the response in the browser.
         # Note: Now is a great time to view the cookies and talk about security concerns
 
 @app.route('/dark_mode', methods=['GET'])
